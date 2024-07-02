4257b082e5381d46d70d964b263fb0513ab388a8
James-Lian <166276178+James-Lian@users.noreply.github.com>
2024-07-02 11:34:58 -0400

    Added content scripts
    Removed old popup scripts, added content scripts and programmatic injection of elements in popup.html
    
    To do: delete popup.html

--- background.js
+++ background.js
@@ -1,10 +1,22 @@
+var hotkey = [];
+
 chrome.commands.onCommand.addListener((command) => {
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
         if (tabs.length > 0) {
             if (command === 'activate-accents') {
+                // example hotkey: Ctrl/Alt + Shift (optional) + letter 
+                // retrieving the hotkey for extension dynamically (in case the user changes it)
+                chrome.commands.getAll((cmds) => {
+                    cmds.forEach((command) => {
+                        if (command.name === "activate-accents") {
+                            hotkey.push(command.shortcut.split("+"))
+                        }
+                    })
+                });
+                // running content.js script
                 chrome.scripting.executeScript({
                     target: {tabId: tabs[0].id, allFrames: true},
-                    files: ['popup.js']
+                    files: ['content.js']
                 })
             }
         }
---
+++ content.js
@@ -0,0 +1,81 @@
+var accentSelection = ["é", "è", "ê", "à", "ç" ,"ù", "«", "»", "ë", "ï", "ü", "â", "ô", "î", "û"];
+
+function init() {
+    const container = document.createElement('div');
+    container.id = "container";
+    
+    const overlay = document.createElement('div');
+    overlay.id = "box";
+
+    container.appendChild(overlay);
+
+    const accents1 = document.createElement('div');
+    accents1.className = 'accent-popup';
+    overlay.appendChild(accents1);
+
+    for (let i=0; i<6; i++) {
+        var accent = document.createElement('h2')
+        accent.innerHTML = accentSelection[i]
+        accent.className = 'accent-text'
+        accent.id = accentSelection[i]
+        accents1.appendChild(accent)
+    }
+
+    const accents2 = document.createElement('div');
+    accents1.className = 'accent-popup';
+    overlay.appendChild(accents2);
+
+    for (let i=6; i<15; i++) {
+        var accent = document.createElement('h2')
+        accent.innerHTML = accentSelection[i]
+        accent.className = 'accent-text'
+        accent.id = accentSelection[i]
+        accents2.appendChild(accent)
+    }
+
+    overlay.style.display = "none";
+}
+
+init()
+
+var currSelection = 0;
+var isActive = false;
+
+/* shorcut pressed */
+document.addEventListener('keydown', (event) => {
+    if (!isActive && (event.altKey || event.ctrlKey) && event.code == hotkey.slice(-1)) {
+        isActive = True
+    }
+    else if (isActive && (event.altKey || event.ctrlKey)) {
+        currSelection = 0;
+        updateOverlay(currSelection);
+    }
+});
+
+/* shortcut or key released */
+document.addEventListener('keyup', (event) => {
+    if (isActive && (event.ctrlKey || event.altKey)) {
+        overlay.style.display = "none";
+        currSelection = 0;
+        isActive = false
+        insertAccent(accentSelection[currSelection])
+    }
+    else if (isActive && (event.code === hotkey.slice(-1))) {
+        currSelection ++;
+        updateOverlay(currSelection);
+    }
+});
+
+function updateOverlay(accentIndex) {
+    overlay.style.display = "block";
+    document.getElementById(accentSelection[accentIndex]).style.backgroundColor = "#ce1221";
+}
+
+function insertAccent(text) {
+    const activeElement = document.activeElement;
+
+    if (activeElement && (activeElement.tagName === "TEXTAREA" || activeElement.tagName === "INPUT")) {
+        const start = activeElement.selectionStart;
+        const end = activeElement.selectionEnd;
+    }
+}
